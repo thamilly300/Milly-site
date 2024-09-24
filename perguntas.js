22d904e6326798b3bf4e1aff960812072ed0a085
thamilly300 <161460493+thamilly300@users.noreply.github.com>
2024-09-24 09:03:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Minecraft',
+    'Qual a empresa do jogo?',
+    'Mojang Studios'
+)
+
+criaCartao(
+    'Minecraft',
+    'Quando o minecraft foi lançado?',
+    '18 de novembro de 2011'
+)
+
+criaCartao(
+    'Minecraft',
+    'Qual o nome do bicho que usamos pra fazer um baú?',
+    'Shulker'
+)
+
+criaCartao(
+    'Minecraft',
+    'Qual era o nome antigo do minecraft?',
+    'Cave Games'
+)
